a5dd5ad4ef59674233bc1a3bbab84d17a44d0e53
Thomas Thorin <thomas.thorin@gmail.com>
2021-11-10 16:35:51 +0100

    added SqlHelper class

---
+++ OOA-Inlamning1/OOA-Inlamning1/Helpers/SqlHelpers.cs
@@ -0,0 +1,32 @@
+﻿namespace OOA_Inlamning1.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Dapper;
+
+    internal static class SqlHelpers
+    {
+        internal static int ExecuteScalar(string sql, string connectionString = "PeopleDB")
+        {
+
+            using (SqlConnection connection = new SqlConnection(ConnectionHelper.CnnStr(connectionString)))
+            {
+                return (int)connection.ExecuteScalar(sql);
+            }
+
+        }
+        internal static void Execute(string sql, string connectionString = "PeopleDB")
+        {
+
+            using (SqlConnection connection = new SqlConnection(ConnectionHelper.CnnStr(connectionString)))
+            {
+                connection.Execute(sql);
+            }
+
+        }
+    }
+}
