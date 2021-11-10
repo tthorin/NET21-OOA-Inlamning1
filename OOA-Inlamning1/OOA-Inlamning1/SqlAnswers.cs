eb48bc6081b7c83d3eed314160b76bbf6eb28416
Thomas Thorin <thomas.thorin@gmail.com>
2021-11-10 17:19:09 +0100

    moved some stuff around

--- OOA-Inlamning1/OOA-Inlamning1/SqlAnswers.cs
+++ OOA-Inlamning1/OOA-Inlamning1/SqlAnswers.cs
@@ -4,13 +4,10 @@
 
     internal static class SqlAnswers
     {
-        private static string dbName = "TT_Net21_People";
-        private static string tableName = "FakePeople";
+        
         internal static void Start()
         {
-            if (!Helpers.DBHelpers.CheckForDB(dbName)) AskToCreateDB();
-            if (Helpers.DBHelpers.CheckForDB(dbName) && !Helpers.DBHelpers.CheckForTable()) AskToCreateTable();
-            if (Helpers.DBHelpers.CheckForDB(dbName) && Helpers.DBHelpers.CheckForTable()) AnswerMenu();
+            if (Helpers.DBHelpers.CheckData()) AnswerMenu();
         }
 
         private static void AnswerMenu()
@@ -18,27 +15,7 @@
             throw new NotImplementedException();
         }
 
-        private static void AskToCreateTable()
-        {
-            string input = "";
-            do
-            {
-                Console.Write($"Table {tableName} doesn't exist, would you like to (c)reate it or (e)xit? ");
-                input = Console.ReadLine().Trim().ToLower();
-            } while (!(input == "c" || input == "create") && !(input == "e" || input == "exit"));
-            if (input[0] == 'c') Helpers.DBHelpers.CreateTable();
-        }
-
-        private static void AskToCreateDB()
-        {   
-            string input = "";
-            do
-            {
-                Console.Write($"Database {dbName} doesn't exist, would you like to (c)reate it or (e)xit? ");
-                input = Console.ReadLine().Trim().ToLower();
-            } while (!(input == "c" || input == "create") && !(input == "e" || input == "exit"));
-            if (input[0] == 'c') Helpers.DBHelpers.CreateDB(dbName);
-        }
+        
     }
 
 }
